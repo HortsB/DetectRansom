f05f3b162e27a81c4da71b628f27ab31c82b66e2
billy <billy006jkr@gmail.com>
2026-03-11 16:24:54 -0500

    add:dashboard component. upd:introducen routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import {MainDashboard} from './dashboard/main-dashboard/main-dashboard';
 
 export const routes: Routes = [
-
+  {
+    path:'',
+    component:MainDashboard,
+  }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from './login/login';
+import {MainDashboard} from './dashboard/main-dashboard/main-dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet, LoginComponent, MainDashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
